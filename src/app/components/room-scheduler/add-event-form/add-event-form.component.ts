53d3ab95f901bdd5d9610a30312889a744c139ce
Josh <jdh428@gmail.com>
2019-05-02 13:59:36 -0400

    updated form

--- src/app/components/room-scheduler/add-event-form/add-event-form.component.ts
+++ src/app/components/room-scheduler/add-event-form/add-event-form.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef } from '@angular/material';
 import { RoomSchedulerComponent } from '../room-scheduler.component';
 import { Batch } from '../../../model/Batch';
 import { Room } from '../../../model/Room';
+import { FormBuilder, Validators } from '@angular/forms';
+import { thisExpression } from '@babel/types';
 
 @Component({
     selector: 'add-event-form',
@@ -20,7 +22,7 @@ export class RoomAddEventFormComponent implements OnInit {
     roomArray = [this.room1, this.room2];
 
     selected = "None";
-    constructor(public dialogRef: MatDialogRef<RoomSchedulerComponent>) {
+    constructor(public dialogRef: MatDialogRef<RoomSchedulerComponent>, private fb: FormBuilder) {
     }
     ngOnInit() {
     }
@@ -32,4 +34,12 @@ export class RoomAddEventFormComponent implements OnInit {
     addEvent() {
         // TODO: Add event with service
     }
+
+    addEventForm = this.fb.group({
+        startDate: ['',Validators.required],
+        endDate: ['', Validators.required],
+        room: ['', Validators.required],
+        batch: ['', Validators.required],
+        other: ['']
+    })
 }
