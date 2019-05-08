0128c384c019e3cf8e1b8b7dd1f67c01acc91a25
Josh <jdh428@gmail.com>
2019-05-08 15:53:30 -0400

    updated for documentation

--- src/app/model/Reservation.ts
+++ src/app/model/Reservation.ts
@@ -1,3 +1,7 @@
+/**
+ * Model for Reservation
+ */
+
 export class Reservation {
     id: number;
     startDate: Date;
@@ -6,6 +10,16 @@ export class Reservation {
     room: number;
     batch: number;
 
+    /**
+     * Creates an instance of Reservation.
+     * @param {number} [id]
+     * @param {Date} [startDate]
+     * @param {Date} [endDate]
+     * @param {string} [description]
+     * @param {number} [room]
+     * @param {number} [batch]
+     * @memberof Reservation
+     */
     constructor(id?: number, startDate?: Date, endDate?: Date, description?: string, room?: number, batch?: number) {
         this.id = id;
         this.startDate = startDate;
