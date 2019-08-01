a6614df91ea4113a5167c1aea83f8447a021dbff
NaemaE <naemaennin@yahoo.com>
2019-08-01 14:44:14 -0500

    Added sprint component

---
+++ src/app/components/add-sprint/add-sprint.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sprint',
+  templateUrl: './add-sprint.component.html',
+  styleUrls: ['./add-sprint.component.css']
+})
+export class AddSprintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
