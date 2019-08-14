f03d68b9a2fbe219adc42d21ea82dd8a261605b8
August Duet <august.duet@revature.com>
2019-08-14 16:11:12 -0400

    tests in final projs.

--- src/app/components/final-projects/final-projects.component.spec.ts
+++ src/app/components/final-projects/final-projects.component.spec.ts
@@ -1,14 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { FinalProjectsComponent } from './final-projects.component';
+import { FinalProjectsComponent } from "./final-projects.component";
 
-describe('FinalProjectsComponent', () => {
+import { RouterModule } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppMaterialModule } from "../../material.module";
+
+import { FinalProjectControllerService } from "../../services/api/final-project-controller/final-project-controller.service";
+import { ProjectItemComponent } from "../final-projects/project-item/project-item.component";
+
+import { batchService, finalProjectService, sprintService } from "../../../jestGlobalMocks";
+import { BatchControllerService } from "../../services/api/batch-controller/batch-controller.service";
+import { AuthService } from "../../services/auth/auth.service";
+import { UrlService } from "../../services/url/url.service";
+import { CachedObjectsService } from "../../services/api/cache/cached-objects.service";
+import { SprintControllerService } from "../../services/api/sprint-controller/sprint-controller.service";
+
+describe("FinalProjectsComponent", () => {
   let component: FinalProjectsComponent;
   let fixture: ComponentFixture<FinalProjectsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FinalProjectsComponent ]
+      imports: [
+        AppMaterialModule,
+        RouterModule,
+        RouterTestingModule,
+      ],
+      declarations: [
+        FinalProjectsComponent,
+        ProjectItemComponent,
+      ],
+      providers: [
+        { provide: FinalProjectControllerService, useValue: finalProjectService },
+        { provide: BatchControllerService, useValue: batchService },
+        { provide: SprintControllerService, useValue: sprintService },
+        AuthService,
+        UrlService,
+        CachedObjectsService,
+      ],
     })
     .compileComponents();
   }));
@@ -19,7 +49,7 @@ describe('FinalProjectsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/jestGlobalMocks.ts
+++ src/jestGlobalMocks.ts
@@ -1,4 +1,4 @@
-import { of } from 'rxjs/observable/of';
+import { of } from "rxjs/observable/of";
 
 export const sprintService = {
   getAll: jest.fn().mockImplementation(() => {
@@ -16,9 +16,9 @@ export const batchService = {
     const batches = [
       {
         id: 740,
-        name: '1803 Mar12 .NET',
-        startDate: new Date('2018-04-11').getTime(),
-        endDate: new Date('2018-06-17').getTime(),
+        name: "1803 Mar12 .NET",
+        startDate: new Date("2018-04-11").getTime(),
+        endDate: new Date("2018-06-17").getTime(),
         curriculum: 2,
         trainer: null,
         cotrainer: null,
@@ -26,13 +26,13 @@ export const batchService = {
         location: 1,
         building: 1,
         room: 7,
-        classSize: 21
+        classSize: 21,
       },
       {
         id: 739,
-        name: '1803 Mar12 .NET',
-        startDate: new Date('2018-04-11').getTime(),
-        endDate: new Date('2018-06-17').getTime(),
+        name: "1803 Mar12 .NET",
+        startDate: new Date("2018-04-11").getTime(),
+        endDate: new Date("2018-06-17").getTime(),
         curriculum: 174,
         trainer: 1,
         cotrainer: 2,
@@ -40,23 +40,23 @@ export const batchService = {
         location: 1,
         building: 1,
         room: 7,
-        classSize: 21
-      }
+        classSize: 21,
+      },
     ];
     return of(batches);
-  })
+  }),
 };
 
 export const curriculumService = {
   findAll: jest.fn().mockImplementation(() => {
     return of(mockCurriculum);
-  })
+  }),
 };
 
 export const skillService = {
   findAll: jest.fn().mockImplementation(() => {
     return of(mockSkills);
-  })
+  }),
 };
 
 export const trainerService = {
@@ -65,19 +65,19 @@ export const trainerService = {
   }),
   create: jest.fn().mockImplementation(() => {
     return of(mockTrainers);
-  })
+  }),
 };
 
 export const settingsService = {
     find: jest.fn().mockImplementation(() => {
         return of(mockSettings);
-    })
-}
+    }),
+};
 
 export const mockSettings = [
   {
     id: 1,
-    alias: 'global',
+    alias: "global",
     trainersPerPage: 0,
     reportGrads: 22,
     batchLength: 10,
@@ -87,523 +87,523 @@ export const mockSettings = [
     trainerBreakDays: 14,
     defaultLocation: null,
     defaultBuilding: null,
-    defaultNamePattern: '$y$m $mmm$d $c'
-  }
+    defaultNamePattern: "$y$m $mmm$d $c"
+  },
 ];
 export const mockSkills = [
   {
     skillId: 4,
-    skillName: 'AngularJ',
-    isActive: true
+    skillName: "AngularJ",
+    isActive: true,
   },
   {
     skillId: 1,
-    skillName: 'Core Java',
-    isActive: true
+    skillName: "Core Java",
+    isActive: true,
   },
   {
     skillId: 2,
-    skillName: 'JUnit',
-    isActive: true
+    skillName: "JUnit",
+    isActive: true,
   },
   {
     skillId: 5,
-    skillName: 'Selenium/WebDriver',
-    isActive: true
+    skillName: "Selenium/WebDriver",
+    isActive: true,
   },
   {
     skillId: 3,
-    skillName: 'Spring',
-    isActive: true
+    skillName: "Spring",
+    isActive: true,
   },
   {
     skillId: 6,
-    skillName: 'XYZ',
-    isActive: true
+    skillName: "XYZ",
+    isActive: true,
   },
   {
     skillId: 32,
-    skillName: 'Salesforce',
-    isActive: true
+    skillName: "Salesforce",
+    isActive: true,
   },
   {
     skillId: 47,
-    skillName: 'Core SDET',
-    isActive: true
+    skillName: "Core SDET",
+    isActive: true,
   },
   {
     skillId: 50,
-    skillName: 'Java Servlets',
-    isActive: true
+    skillName: "Java Servlets",
+    isActive: true,
   },
   {
     skillId: 51,
-    skillName: 'JSP',
-    isActive: true
+    skillName: "JSP",
+    isActive: true,
   },
   {
     skillId: 78,
-    skillName: 'C++',
-    isActive: true
+    skillName: "C++",
+    isActive: true,
   },
   {
     skillId: 25,
-    skillName: 'Oracle SQL',
-    isActive: true
+    skillName: "Oracle SQL",
+    isActive: true,
   },
   {
     skillId: 48,
-    skillName: 'JDBC',
-    isActive: true
+    skillName: "JDBC",
+    isActive: true,
   },
   {
     skillId: 49,
-    skillName: 'HTML',
-    isActive: true
+    skillName: "HTML",
+    isActive: true,
   },
   {
     skillId: 52,
-    skillName: 'Freemarker',
-    isActive: true
+    skillName: "Freemarker",
+    isActive: true,
   },
   {
     skillId: 53,
-    skillName: 'CSS',
-    isActive: true
+    skillName: "CSS",
+    isActive: true,
   },
   {
     skillId: 54,
-    skillName: 'jQuery',
-    isActive: true
+    skillName: "jQuery",
+    isActive: true,
   },
   {
     skillId: 55,
-    skillName: 'Hibernate',
-    isActive: true
+    skillName: "Hibernate",
+    isActive: true,
   },
   {
     skillId: 56,
-    skillName: 'REST',
-    isActive: true
+    skillName: "REST",
+    isActive: true,
   },
   {
     skillId: 57,
-    skillName: 'SOAP',
-    isActive: true
+    skillName: "SOAP",
+    isActive: true,
   },
   {
     skillId: 59,
-    skillName: 'ASP.NET MVC',
-    isActive: true
+    skillName: "ASP.NET MVC",
+    isActive: true,
   },
   {
     skillId: 60,
-    skillName: 'ASP.NET WEB API',
-    isActive: true
+    skillName: "ASP.NET WEB API",
+    isActive: true,
   },
   {
     skillId: 58,
-    skillName: 'C#',
-    isActive: true
+    skillName: "C#",
+    isActive: true,
   },
   {
     skillId: 66,
-    skillName: 'ADO.NET',
-    isActive: true
+    skillName: "ADO.NET",
+    isActive: true,
   },
   {
     skillId: 62,
-    skillName: 'MSTest',
-    isActive: true
+    skillName: "MSTest",
+    isActive: true,
   },
   {
     skillId: 67,
-    skillName: 'T-SQL',
-    isActive: true
+    skillName: "T-SQL",
+    isActive: true,
   },
   {
     skillId: 65,
-    skillName: 'Entity Framework',
-    isActive: true
+    skillName: "Entity Framework",
+    isActive: true,
   },
   {
     skillId: 63,
-    skillName: 'xUnit',
-    isActive: true
+    skillName: "xUnit",
+    isActive: true,
   },
   {
     skillId: 64,
-    skillName: 'NLog',
-    isActive: true
+    skillName: "NLog",
+    isActive: true,
   },
   {
     skillId: 61,
-    skillName: 'IIS',
-    isActive: true
+    skillName: "IIS",
+    isActive: true,
   },
   {
     skillId: 69,
-    skillName: 'TestNG',
-    isActive: true
+    skillName: "TestNG",
+    isActive: true,
   },
   {
     skillId: 72,
-    skillName: 'Manual Testing',
-    isActive: true
+    skillName: "Manual Testing",
+    isActive: true,
   },
   {
     skillId: 70,
-    skillName: 'Pyhton',
-    isActive: true
+    skillName: "Pyhton",
+    isActive: true,
   },
   {
     skillId: 71,
-    skillName: 'UFT',
-    isActive: true
+    skillName: "UFT",
+    isActive: true,
   },
   {
     skillId: 68,
-    skillName: 'Cucumber',
-    isActive: true
+    skillName: "Cucumber",
+    isActive: true,
   },
   {
     skillId: 73,
-    skillName: 'C',
-    isActive: true
+    skillName: "C",
+    isActive: true,
   },
   {
     skillId: 46,
-    skillName: 'Core .NET',
-    isActive: true
+    skillName: "Core .NET",
+    isActive: true,
   },
   {
     skillId: 26,
-    skillName: 'Microservices',
-    isActive: true
+    skillName: "Microservices",
+    isActive: true,
   },
   {
     skillId: 27,
-    skillName: 'Big Data',
-    isActive: true
+    skillName: "Big Data",
+    isActive: true,
   },
   {
     skillId: 28,
-    skillName: 'Dynamics CRM',
-    isActive: true
+    skillName: "Dynamics CRM",
+    isActive: true,
   },
   {
     skillId: 29,
-    skillName: 'Oracle Fusion',
-    isActive: true
+    skillName: "Oracle Fusion",
+    isActive: true,
   },
   {
     skillId: 30,
-    skillName: 'PEGA',
-    isActive: true
+    skillName: "PEGA",
+    isActive: true,
   },
   {
     skillId: 79,
-    skillName: 'Go',
-    isActive: true
-  }
+    skillName: "Go",
+    isActive: true,
+  },
 ];
 export const mockCurriculum = [
   {
     id: 2,
-    name: '.NET',
+    name: ".NET",
     isActive: true,
     isCore: 1,
     skills: [
       {
         skillId: 59,
-        skillName: 'ASP.NET MVC',
-        isActive: true
+        skillName: "ASP.NET MVC",
+        isActive: true,
       },
       {
         skillId: 60,
-        skillName: 'ASP.NET WEB API',
-        isActive: true
+        skillName: "ASP.NET WEB API",
+        isActive: true,
       },
       {
         skillId: 58,
-        skillName: 'C#',
-        isActive: true
+        skillName: "C#",
+        isActive: true,
       },
       {
         skillId: 66,
-        skillName: 'ADO.NET',
-        isActive: true
+        skillName: "ADO.NET",
+        isActive: true,
       },
       {
         skillId: 62,
-        skillName: 'MSTest',
-        isActive: true
+        skillName: "MSTest",
+        isActive: true,
       },
       {
         skillId: 65,
-        skillName: 'Entity Framework',
-        isActive: true
+        skillName: "Entity Framework",
+        isActive: true,
       },
       {
         skillId: 63,
-        skillName: 'xUnit',
-        isActive: true
+        skillName: "xUnit",
+        isActive: true,
       },
       {
         skillId: 64,
-        skillName: 'NLog',
-        isActive: true
+        skillName: "NLog",
+        isActive: true,
       },
       {
         skillId: 61,
-        skillName: 'IIS',
-        isActive: true
+        skillName: "IIS",
+        isActive: true,
       },
       {
         skillId: 46,
-        skillName: 'Core .NET',
-        isActive: true
-      }
-    ]
+        skillName: "Core .NET",
+        isActive: true,
+      },
+    ],
   },
   {
     id: 1,
-    name: 'Java',
+    name: "Java",
     isActive: true,
     isCore: 1,
     skills: [
       {
-        skillId: 1
+        skillId: 1,
       },
       {
-        skillId: 2
+        skillId: 2,
       },
       {
-        skillId: 3
+        skillId: 3,
       },
       {
-        skillId: 4
+        skillId: 4,
       },
       {
-        skillId: 25
+        skillId: 25,
       },
       {
-        skillId: 48
+        skillId: 48,
       },
       {
-        skillId: 49
+        skillId: 49,
       },
       {
-        skillId: 50
+        skillId: 50,
       },
       {
-        skillId: 51
+        skillId: 51,
       },
       {
-        skillId: 52
+        skillId: 52,
       },
       {
-        skillId: 54
+        skillId: 54,
       },
       {
-        skillId: 55
+        skillId: 55,
       },
       {
-        skillId: 56
+        skillId: 56,
       },
       {
-        skillId: 57
-      }
-    ]
+        skillId: 57,
+      },
+    ],
   },
   {
     id: 3,
-    name: 'SDET',
+    name: "SDET",
     isActive: true,
     isCore: 1,
     skills: [
       {
-        skillId: 47
+        skillId: 47,
       },
       {
-        skillId: 68
+        skillId: 68,
       },
       {
-        skillId: 69
+        skillId: 69,
       },
       {
-        skillId: 5
+        skillId: 5,
       },
       {
-        skillId: 70
+        skillId: 70,
       },
       {
-        skillId: 71
+        skillId: 71,
       },
       {
-        skillId: 72
-      }
-    ]
+        skillId: 72,
+      },
+    ],
   },
   {
     id: 105,
-    name: 'Custom',
+    name: "Custom",
     isActive: true,
     isCore: 1,
-    skills: []
+    skills: [],
   },
   {
     id: 25,
-    name: 'Appian',
-    skills: []
+    name: "Appian",
+    skills: [],
   },
   {
     id: 4,
-    name: 'Capital One',
+    name: "Capital One",
     isActive: true,
     isCore: 0,
     skills: [
       {
-        skillId: 4
+        skillId: 4,
       },
       {
-        skillId: 3
+        skillId: 3,
       },
       {
-        skillId: 55
+        skillId: 55,
       },
       {
-        skillId: 56
-      }
-    ]
+        skillId: 56,
+      },
+    ],
   },
   {
     id: 64,
-    name: 'Testing',
+    name: "Testing",
     isActive: false,
     isCore: 0,
     skills: [
       {
-        skillId: 47
+        skillId: 47,
       },
       {
-        skillId: 50
+        skillId: 50,
       },
       {
-        skillId: 51
-      }
-    ]
+        skillId: 51,
+      },
+    ],
   },
   {
     id: 65,
-    name: 'Testing2',
+    name: "Testing2",
     isActive: false,
     isCore: 0,
     skills: [
       {
-        skillId: 4
+        skillId: 4,
       },
       {
-        skillId: 47
+        skillId: 47,
       },
       {
-        skillId: 50
+        skillId: 50,
       },
       {
-        skillId: 51
-      }
-    ]
+        skillId: 51,
+      },
+    ],
   },
   {
     id: 24,
-    name: 'name',
+    name: "name",
     isActive: false,
     isCore: 0,
     skills: [
       {
-        skillId: 4
+        skillId: 4,
       },
       {
-        skillId: 1
+        skillId: 1,
       },
       {
-        skillId: 47
+        skillId: 47,
       },
       {
-        skillId: 50
+        skillId: 50,
       },
       {
-        skillId: 51
-      }
-    ]
+        skillId: 51,
+      },
+    ],
   },
   {
     id: 5,
-    name: 'Big Data',
+    name: "Big Data",
     isActive: false,
     isCore: 0,
     skills: [
       {
-        skillId: 27
-      }
-    ]
+        skillId: 27,
+      },
+    ],
   },
   {
     id: 6,
-    name: 'Dynamics CRM',
+    name: "Dynamics CRM",
     isActive: true,
     isCore: 0,
     skills: [
       {
-        skillId: 28
-      }
-    ]
+        skillId: 28,
+      },
+    ],
   },
   {
     id: 7,
-    name: 'PEGA',
+    name: "PEGA",
     isActive: true,
     isCore: 0,
     skills: [
       {
-        skillId: 30
-      }
-    ]
+        skillId: 30,
+      },
+    ],
   },
   {
     id: 8,
-    name: 'Oracle Fusion',
+    name: "Oracle Fusion",
     isActive: true,
     isCore: 0,
     skills: [
       {
-        skillId: 29
-      }
-    ]
+        skillId: 29,
+      },
+    ],
   },
   {
     id: 9,
-    name: 'GO',
+    name: "GO",
     isActive: true,
     isCore: 0,
     skills: [
       {
-        skillId: 31
-      }
-    ]
+        skillId: 31,
+      },
+    ],
   },
   {
     id: 10,
-    name: 'Salesforce',
-    skills: []
+    name: "Salesforce",
+    skills: [],
   },
   {
     id: 44,
-    name: 'BA',
+    name: "BA",
     isActive: true,
     isCore: 0,
-    skills: []
-  }
+    skills: [],
+  },
 ];
 export const mockTrainers = [
   {
@@ -615,34 +615,34 @@ export const mockTrainers = [
     id: 1,
     skills: [
       {
-        skillId: 4
+        skillId: 4,
       },
       {
-        skillId: 1
+        skillId: 1,
       },
       {
-        skillId: 2
+        skillId: 2,
       },
       {
-        skillId: 50
+        skillId: 50,
       },
       {
-        skillId: 51
+        skillId: 51,
       },
       {
-        skillId: 5
-      }
+        skillId: 5,
+      },
     ],
-    preferredLocation: 4
+    preferredLocation: 4,
   },
   {
-    "id": 2,
-    "firstName": "Fred",
-    "lastName": "Belotte",
-    "isActive": true,
-    "resume": null,
-    "email": "fred.belotte@revature.com",
-    "skills": []
+    id: 2,
+    firstName: "Fred",
+    lastName: "Belotte",
+    isActive: true,
+    resume: null,
+    email: "fred.belotte@revature.com",
+    skills: [],
   },
   {
     id: 37,
@@ -651,7 +651,7 @@ export const mockTrainers = [
     isActive: true,
     resume: null,
     email: "",
-    skills: []
+    skills: [],
   },
   {
     id: 38,
@@ -660,7 +660,7 @@ export const mockTrainers = [
     isActive: true,
     resume: null,
     email: "",
-    skills: []
+    skills: [],
   },
   {
     id: 115,
@@ -669,6 +669,6 @@ export const mockTrainers = [
     isActive: true,
     resume: null,
     email: "",
-    skills: []
-  }
-]
+    skills: [],
+  },
+];
