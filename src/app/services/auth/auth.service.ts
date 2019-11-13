b386201950370ca31614b87899e1a778d41ee153
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2019-11-13 12:54:00 -0500

    adding reset functionality

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { UrlService } from '../url/url.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import Auth0Lock from 'auth0-lock';
 import * as auth0 from 'auth0-js';
@@ -8,15 +9,17 @@ import { Trainer } from '../../model/Trainer';
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 @Injectable()
 export class AuthService {
-  constructor(private router: Router, private urlService: UrlService) {}
-
+  changePasswordUrl = "http://localhost:4200/change-password/";
+  authUrl = "http://localhost:5000/api/auth/";
+  constructor(private router: Router, private urlService: UrlService, private http: HttpClient) {}
   //Userpool information for connection
   poolData = {
-    UserPoolId: 'us-east-1_hE8EafqgV',
+    UserPoolId:  'us-east-1_hE8EafqgV', //revature
     ClientId: '4vd3i79p1ml95c14vkfl9i7567'
+    //UserPoolId: 'us-east-2_bAaqirY4q', //personal
+    //ClientId: '1r4vaek14a6qkouvf2udcklo6f'
   };
   userPool = new AmazonCognitoIdentity.CognitoUserPool(this.poolData);
-
   //Performs authentication with user entered information
   public startLogin(email: string, password: string) {
     const userData = {
@@ -32,39 +35,57 @@ export class AuthService {
     const router = this.router;
     const urlService = this.urlService;
     const onSuccess = result => {
-      const expiresAt = JSON.stringify(result.getAccessToken().getExpiration() * 1000 + new Date().getTime());
-      localStorage.setItem('access_token', result.getAccessToken().getJwtToken());
-      localStorage.setItem('id_token', result.getIdToken().getJwtToken());
-      localStorage.setItem('expires_at', expiresAt);
-      if (authenticationData.Username.includes('svp')) {
-        localStorage.setItem('roles', 'SVP of Technology');
-      } else {
-        localStorage.setItem('roles', 'Trainer');
-      }
-      localStorage.setItem('email', authenticationData.Username);
-      router.navigate([urlService.getOverviewUrl()]);
-    };
-    const onFailure = err => {
-      console.log('Error upon trying to login...');
-      alert(err);
-      router.navigate([urlService.getLoginUrl()]);
-    };
-
-    const newPasswordRequired = vUser => {
-      console.log(password);
-      cognitoUser.completeNewPasswordChallenge('P@$$w0rd123', [], {
-        onSuccess,
-        onFailure
-      });
-    };
+    console.log("Successful login...");
+    const expiresAt = JSON.stringify(result.getAccessToken().getExpiration() * 1000 + new Date().getTime());
+    localStorage.setItem('access_token', result.getAccessToken().getJwtToken());
+    localStorage.setItem('id_token', result.getIdToken().getJwtToken());
+    localStorage.setItem('expires_at', expiresAt);
+    //if (authenticationData.Username.includes('svp')){
+      //localStorage.setItem('roles', 'SVP of Technology');
+    //} else {
+      //localStorage.setItem('roles', 'Trainer');
+    //}
+    localStorage.setItem('roles', result.getIdToken().payload.roles);
+    localStorage.setItem('email', authenticationData.Username);
+    router.navigate([urlService.getOverviewUrl()]);};
+  const onFailure = (err) => {
+    console.log("Error upon trying to login...");
+    alert("Error upon trying to login...");
+    router.navigate([urlService.getLoginUrl()]); };
+    //const newPasswordRequired = vUser => {
+      //console.log(password);
+      //const newPassword = passwordChallenge();
+      //cognitoUser.completeNewPasswordChallenge(newPassword, [], {onSuccess, onFailure
+      //});
+    //};
     cognitoUser.authenticateUser(authenticationDetails, {
       onSuccess,
       onFailure,
-      newPasswordRequired
-    });
+      }
+    );
   }
-
-  //Removes user information from current session
+  public forgotPassword() {
+    let username = prompt("Enter your Email");
+    const userData = {
+      Username: username,
+      Pool: this.userPool,
+    };
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.forgotPassword({
+      onSuccess: (result) => {
+      console.log('call result: ' + result);
+    },
+    onFailure: (err) => {
+      alert(JSON.stringify(err));
+      // console.log(err);
+    },
+    inputVerificationCode() {
+      const verificationCode = prompt('Please input verification code ' ,'');
+      const newPassword = prompt('Enter new password ' ,'');
+      cognitoUser.confirmPassword(verificationCode, newPassword, this);
+    }
+  });
+  }
   public logout(): void {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
@@ -73,14 +94,12 @@ export class AuthService {
     localStorage.removeItem('email');
     this.router.navigate([this.urlService.getLoginUrl()]);
   }
-
   // Check whether the current time is past the Access Token's expiry time
   public isAuthenticated(): boolean {
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
   }
-
-  //Checks role of current session
+  //Checks role of current session and should return either a single role or multiple roles (roles based access)
   public userHasRole(expectedRoles: Array<string>): boolean {
     const userRoles = localStorage.getItem('roles');
     if (!this.isAuthenticated()) {
@@ -88,9 +107,9 @@ export class AuthService {
     }
     return expectedRoles.every(value => userRoles.indexOf(value) >= 0);
   }
-
   //Gets stored access token
   public getToken(): string {
     return localStorage.getItem('access_token');
   }
+
 }
